d925cffcaa413cbfe0c3fcc546d11a10e174865f
tiagoaguiar100 <tiagoaguiar100@gmail.com>
2013-04-04 10:27:59 +0100

    Added Exception NotEnoughDataServers

---
+++ PADIFS-Project/SharedLibrary/Exceptions/NotEnoughDataServersException.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Exceptions
+{
+    [Serializable]
+    public class NotEnoughDataServersException : ApplicationException
+    {
+        private int nbDataServersRequested;
+        private int nbDataServersRunning;
+
+        public NotEnoughDataServersException() { }
+
+        public NotEnoughDataServersException(int nbDataServersRequested, int nbDataServersRunning)
+            : base("Not enough DataServers. Requested: " + nbDataServersRequested + " Running: " + nbDataServersRunning)
+        {
+            this.nbDataServersRequested = nbDataServersRequested;
+            this.nbDataServersRunning = nbDataServersRunning;
+        }
+
+        public NotEnoughDataServersException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+
+        public int NbDataServersRequested
+        {
+            get { return this.nbDataServersRequested; }
+        }
+
+        public int NbDataServersRunning
+        {
+            get { return this.nbDataServersRunning; }
+        }
+    }
+}
